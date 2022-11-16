6d043edb04f40f1ae5fe429a0f61fa5b180787b3
Taras Dyda <taras.dyda@bmigroup.com>
2022-11-16 22:41:18 +0200

    remove is window defined method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { isSSR } from "./isWindowDefined";
 import { renderGround } from "./renderGround";
 import { renderPyramid } from "./renderPyramid";
 import { renderSky } from "./renderSky";
 import { renderSun } from "./renderSun";
 
-export { isSSR, renderGround, renderPyramid, renderSky, renderSun };
+export { renderGround, renderPyramid, renderSky, renderSun };
